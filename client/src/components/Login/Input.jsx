cb5fd42dd7f3c9a094938a08bc45bf452fb9a27d
Shashank <shashank1306s@gmail.com>
2022-01-29 10:28:58 +0530

    Login and Sign Up component added

---
+++ client/src/components/Login/Input.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const Input = (props) => {
+    console.log("type: ", props.type);
+    return (
+        <Grid item xs={12} sm={props.half ? 6 : 12}>
+            <TextField
+                name={props.name}
+                onChange={props.handleChange}
+                variant="outlined"
+                required
+                fullWidth
+                label={props.label}
+                autoFocus={props.autoFocus}
+                type={props.type}
+                InputProps={props.name === 'password' ? {
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton onClick={props.handleShowPassword}>
+                                {props.type === 'password' ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                } : null}
+            />
+        </Grid>
+    )
+};
+
+export default Input;
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,16 +1,62 @@
-import { Button, Container, Paper } from '@material-ui/core';
-import React from 'react';
+import { Button, Container, Paper, Avatar, Typography, Grid } from '@material-ui/core';
+import React, { useState } from 'react';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
+import useStyles from './styles';
 
 const Login = () => {
-  return (<>
-      <Container component="main" maxWidth='xs'>
-        <Paper elevation={3}>
-            <form>
-                <Button>Sign on</Button>
-            </form>
-        </Paper>
-      </Container>
-  </>)
+    const [isSignup, setIsSignup] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const classes = useStyles();
+
+    const handleSubmit = (e) => {
+       
+      };
+    
+    const handleChange = (e) => {
+       
+    };  
+
+    const handleShowPassword = () => setShowPassword(!showPassword);
+
+    const switchMode = () => {
+        setIsSignup(!isSignup);
+        setShowPassword(false);
+    }
+
+    return (<>
+        <Container component="main" maxWidth='xs'>
+            <Paper className={classes.paper} elevation={3}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        { isSignup && (
+                            <>
+                                <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half/>
+                                <Input name="lastName" label="Last Name" handleChange={handleChange} half/>
+                            </>
+                        )}
+                        <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="Password" handleChange={handleChange} type = {showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                        {isSignup && <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type='password' />}
+                    </Grid>
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        {isSignup ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <Grid container justify="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign up"}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
+    </>)
 };
 
 export default Login;
