b36279544ad6866827735d7c8cadc9a3358fe011
Shashank <shashank1306s@gmail.com>
2022-01-31 19:14:52 +0530

    security leak fixed

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -14,8 +16,7 @@ app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-const CONNECTION_URL = 'mongodb+srv://shashank:shashank13@cluster0.emksi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT} and Database connection is successfull`)))
 .catch((error) => console.log(error));
